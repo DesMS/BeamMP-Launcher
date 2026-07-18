6ea18c24b96d5247e429a81375646071e93cd5c3
DesMS <84988422+DesMS@users.noreply.github.com>
2026-07-18 17:48:50 -0500

    Follow best practices in compile_installer.sh

--- compile_installer.sh
+++ compile_installer.sh
@@ -12,15 +12,15 @@ cp installer.sh ./BeamMP-Installer
 # Check for libraries automagically
 if command -v ldd > /dev/null; then
     echo "Adding checks for libraries"
-    export LIBS="$(ldd ./BeamMP-Launcher | awk '{print $1}' | grep -v "linux-vdso.so" | grep -v "ld-linux")"
-    export LIBREQUEST="export LIBREQUEST=\"\""
+    LIBS="$(ldd ./BeamMP-Launcher | awk '{print $1}' | grep -v "linux-vdso.so" | grep -v "ld-linux")"
+    LIBREQUEST="LIBREQUEST=\"\""
     while IFS= read -r LIB; do
-        export LIBREQUEST="$LIBREQUEST\nif [ ! -n \"\$(ldconfig -p | grep \"$LIB\")\" ]; then\nexport LIBREQUEST=\"$LIB \$LIBREQUEST\"\nfi"
+        LIBREQUEST="$LIBREQUEST\nif [ ! -n \"\$(ldconfig -p | grep \"$LIB\")\" ]; then\nLIBREQUEST=\"$LIB \$LIBREQUEST\"\nfi"
     done <<< "$LIBS"
     sed -i "s/SETUPLIBREQUEST/$LIBREQUEST/g" ./BeamMP-Installer
 else
     echo "Skipping library checks due to lack of ldd (What system are you running?)"
-    sed -i "s/SETUPLIBREQUEST/export LIBREQUEST=\"\"/g" ./BeamMP-Installer
+    sed -i "s/SETUPLIBREQUEST/LIBREQUEST=\"\"/g" ./BeamMP-Installer
 fi
 
 # Minify
@@ -33,20 +33,20 @@ sed -i '/^[[:blank:]]*$/d' ./BeamMP-Installer # Remove empty lines (ALWAYS USE \
 # Add binaries
 echo "Adding and linking files to script"
 echo -e "\nexit" >> ./BeamMP-Installer
-export LINES0="$(wc -l < ./BeamMP-Installer)"
-export LINES1="$(wc -l < ./BeamMP-Launcher)"
+LINES0="$(wc -l < ./BeamMP-Installer)"
+LINES1="$(wc -l < ./BeamMP-Launcher)"
 sed -i "s/+FILELINELENGTH0/+$((LINES0+1))/g" ./BeamMP-Installer
 sed -i "s/OUTFILELINELENGTH0/$((LINES1+1))/g" ./BeamMP-Installer
 cat "./BeamMP-Launcher" >> ./BeamMP-Installer
 echo >> ./BeamMP-Installer
-export LINES0="$(wc -l < ./BeamMP-Installer)"
-export LINES1="$(wc -l < ./BeamMP.ico)"
+LINES0="$(wc -l < ./BeamMP-Installer)"
+LINES1="$(wc -l < ./BeamMP.ico)"
 sed -i "s/+FILELINELENGTH1/+$((LINES0+1))/g" ./BeamMP-Installer
 sed -i "s/OUTFILELINELENGTH1/$((LINES1+1))/g" ./BeamMP-Installer
 cat "./BeamMP.ico" >> ./BeamMP-Installer
 echo >> ./BeamMP-Installer
-export LINES0="$(wc -l < ./BeamMP-Installer)"
-export LINES1="$(wc -l < ./BeamMP.desktop)"
+LINES0="$(wc -l < ./BeamMP-Installer)"
+LINES1="$(wc -l < ./BeamMP.desktop)"
 sed -i "s/+FILELINELENGTH2/+$((LINES0+1))/g" ./BeamMP-Installer
 sed -i "s/OUTFILELINELENGTH2/$((LINES1+1))/g" ./BeamMP-Installer
 cat "./BeamMP.desktop" >> ./BeamMP-Installer
