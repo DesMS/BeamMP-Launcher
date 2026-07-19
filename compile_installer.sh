dfb2d2889b25fb41e4264ec5d7e1040d2545a063
DesMS <84988422+DesMS@users.noreply.github.com>
2026-07-18 23:19:28 -0500

    Change from BeamMP-Installer to BeamMP_Installer.sh
    This allows for clarity and matches the current Windows BeamMP installer (BeamMP_Installer.exe)

--- compile_installer.sh
+++ compile_installer.sh
@@ -7,7 +7,7 @@ fi
 echo "Setting up"
 cp ./bin/BeamMP-Launcher ./data
 cd data
-cp installer.sh ./BeamMP-Installer
+cp installer.sh ./BeamMP_Installer.sh
 
 # Check for libraries automagically
 if command -v ldd > /dev/null; then
@@ -17,42 +17,41 @@ if command -v ldd > /dev/null; then
     while IFS= read -r LIB; do
         LIBREQUEST="$LIBREQUEST\nif [ ! -n \"\$(ldconfig -p | grep \"$LIB\")\" ]; then\nLIBREQUEST=\"$LIB \$LIBREQUEST\"\nfi"
     done <<< "$LIBS"
-    sed -i "s/SETUPLIBREQUEST/$LIBREQUEST/g" ./BeamMP-Installer
+    sed -i "s/SETUPLIBREQUEST/$LIBREQUEST/g" ./BeamMP_Installer.sh
 else
-    echo "Skipping library checks due to lack of ldd (What system are you running?)"
-    sed -i "s/SETUPLIBREQUEST/LIBREQUEST=\"\"/g" ./BeamMP-Installer
+    echo "Skipping library checks due to lack of ldd (What system are you running???)"
+    sed -i "s/SETUPLIBREQUEST/LIBREQUEST=\"\"/g" ./BeamMP_Installer.sh
 fi
 
 # Minify
 echo "Cleaning up file"
-# sed -i -e '/^\s*#.*$/d' -e '/^\s*$/d' ./BeamMP-Installer
-sed -i '/^[[:blank:]]*#/d;s/#.*//' ./BeamMP-Installer # Remove comments
-sed -i 's/^[[:space:]]*//g' ./BeamMP-Installer # Remove whitespace
-sed -i '/^[[:blank:]]*$/d' ./BeamMP-Installer # Remove empty lines (ALWAYS USE \n!!)
+sed -i '/^[[:blank:]]*#/d;s/#.*//' ./BeamMP_Installer.sh # Remove comments
+sed -i 's/^[[:space:]]*//g' ./BeamMP_Installer.sh # Remove whitespace
+sed -i '/^[[:blank:]]*$/d' ./BeamMP_Installer.sh # Remove empty lines (ALWAYS USE \n!!)
 
 # Add binaries
 echo "Adding and linking files to script"
-echo -e "\nexit" >> ./BeamMP-Installer
-LINES0="$(wc -l < ./BeamMP-Installer)"
+echo -e "\nexit" >> ./BeamMP_Installer.sh
+LINES0="$(wc -l < ./BeamMP_Installer.sh)"
 LINES1="$(wc -l < ./BeamMP-Launcher)"
-sed -i "s/+FILELINELENGTH0/+$((LINES0+1))/g" ./BeamMP-Installer
-sed -i "s/OUTFILELINELENGTH0/$((LINES1+1))/g" ./BeamMP-Installer
-cat "./BeamMP-Launcher" >> ./BeamMP-Installer
-echo >> ./BeamMP-Installer
-LINES0="$(wc -l < ./BeamMP-Installer)"
+sed -i "s/+FILELINELENGTH0/+$((LINES0+1))/g" ./BeamMP_Installer.sh
+sed -i "s/OUTFILELINELENGTH0/$((LINES1+1))/g" ./BeamMP_Installer.sh
+cat "./BeamMP-Launcher" >> ./BeamMP_Installer.sh
+echo >> ./BeamMP_Installer.sh
+LINES0="$(wc -l < ./BeamMP_Installer.sh)"
 LINES1="$(wc -l < ./BeamMP.ico)"
-sed -i "s/+FILELINELENGTH1/+$((LINES0+1))/g" ./BeamMP-Installer
-sed -i "s/OUTFILELINELENGTH1/$((LINES1+1))/g" ./BeamMP-Installer
-cat "./BeamMP.ico" >> ./BeamMP-Installer
-echo >> ./BeamMP-Installer
-LINES0="$(wc -l < ./BeamMP-Installer)"
+sed -i "s/+FILELINELENGTH1/+$((LINES0+1))/g" ./BeamMP_Installer.sh
+sed -i "s/OUTFILELINELENGTH1/$((LINES1+1))/g" ./BeamMP_Installer.sh
+cat "./BeamMP.ico" >> ./BeamMP_Installer.sh
+echo >> ./BeamMP_Installer.sh
+LINES0="$(wc -l < ./BeamMP_Installer.sh)"
 LINES1="$(wc -l < ./BeamMP.desktop)"
-sed -i "s/+FILELINELENGTH2/+$((LINES0+1))/g" ./BeamMP-Installer
-sed -i "s/OUTFILELINELENGTH2/$((LINES1+1))/g" ./BeamMP-Installer
-cat "./BeamMP.desktop" >> ./BeamMP-Installer
+sed -i "s/+FILELINELENGTH2/+$((LINES0+1))/g" ./BeamMP_Installer.sh
+sed -i "s/OUTFILELINELENGTH2/$((LINES1+1))/g" ./BeamMP_Installer.sh
+cat "./BeamMP.desktop" >> ./BeamMP_Installer.sh
 
 # Finish up
 echo "Finishing up"
-chmod u+x ./BeamMP-Installer
+chmod u+x ./BeamMP_Installer.sh
 mkdir ../bin &>/dev/null
-mv ./BeamMP-Installer ../bin
+mv ./BeamMP_Installer.sh ../bin
